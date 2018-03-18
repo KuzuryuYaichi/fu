c945bbdabe819bebc248b225aa74ff31bd7c2292
彧涵 刘 <2731820096@qq.com>
2018-03-18 23:42:50 +0800

    Third

--- fu/Algorithm/Sort.cpp
+++ fu/Algorithm/Sort.cpp
@@ -15,4 +15,16 @@ namespace fu_sort
 			}
 		}
 	}
+
+	template <typename T>
+	inline constexpr void searchSort(T[] arr, const size_t size)
+	{
+		for (auto i = 0; i < size - 1; i++)
+		{
+			for (auto j = i + 1; j < size; j++)
+			{
+				if (arr[i] > arr[j + 1])swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
 }
