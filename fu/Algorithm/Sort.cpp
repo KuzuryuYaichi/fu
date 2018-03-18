25174340c8e36a2891162c8ea6243835c0019aae
彧涵 刘 <2731820096@qq.com>
2018-03-18 23:24:50 +0800

    On Another

---
+++ fu/Algorithm/Sort.cpp
@@ -0,0 +1,18 @@
+#include "../Algorithm/Util.hpp"
+
+using namespace fu_util::swap;
+
+namespace fu_sort
+{
+	template <typename T>
+	inline constexpr void bubbleSort(T[] arr, const size_t size)
+	{
+		for (auto i = 0; i < size - 1; i++)
+		{
+			for (auto j = 0; j < size - i - 1; j++)
+			{
+				if (arr[j] > arr[j + 1])swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
